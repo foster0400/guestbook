71cd7a496a37c7361d27dee6bc5a115892fb096d
foster0400 <chanchan0400@gmail.com>
2021-08-02 10:43:15 +0700

    create databases

---
+++ database/migrations/2021_08_01_145228_create_signs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('signs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('event_id');
+            $table->unsignedBigInteger('signer_id');
+            $table->string('address');
+            $table->string('message');
+
+            $table->foreign('signer_id')->references('id')->on('users');
+            $table->foreign('event_id')->references('id')->on('events');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('signs');
+    }
+}
