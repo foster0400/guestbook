369fa8bf3414afbab2a326d24addce94d724513b
foster0400 <chanchan0400@gmail.com>
2021-08-02 13:56:57 +0700

    edit search function

--- app/Http/Controllers/GuestbookController.php
+++ app/Http/Controllers/GuestbookController.php
@@ -11,7 +11,19 @@ class GuestbookController extends Controller
     public function search(Request $request)
     {
         $eventId = $request->eventId;
-        $event = Event::where('id','=',$eventId)->get();
-        return view('sign', ['event' => $event,'eventId'=>$eventId]);
+        $event=NULL;
+        $message=NULL;
+        $acceptance=NULL;
+        // if user has input eventId
+        if($eventId){
+            $event = Event::where('id','=',$eventId)->first();
+            #check inputted eventId exist or not
+            if($event){
+                $acceptance=$event->isOpened;
+            }else{
+                $message="NOT FOUND";
+            }
+        }
+        return view('sign', ['event' => $event,'eventId'=>$eventId,'message'=>$message,'acceptance'=>$acceptance]);
     }
 }
