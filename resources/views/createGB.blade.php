a90d375da6579b7a61c309b1efd9fe1a5b588640
foster0400 <chanchan0400@gmail.com>
2021-08-02 22:12:59 +0700

    change name to title

--- resources/views/createGB.blade.php
+++ resources/views/createGB.blade.php
@@ -6,12 +6,12 @@
             @csrf
 
             <div class="form-group row">
-                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                 <div class="col-md-6">
-                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                    <input id="title" type="text" class="form-control @error('name') is-invalid @enderror" name="title" value="{{ old('title') }}" required autofocus>
 
-                    @error('name')
+                    @error('title')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
