89d0f29054fe428b693c3382c0cbde16d15eb028
foster0400 <chanchan0400@gmail.com>
2021-08-03 13:39:58 +0700

    update sign view

--- resources/views/sign.blade.php
+++ resources/views/sign.blade.php
@@ -10,39 +10,74 @@
 @endif
 
 <div class="container">
-    <form method="GET" action="/sign-guestbook" class="form-inline my-2" style="width:60%">
+    <form method="GET" action="/sign-guestbook" class="form-inline my-2 row justify-content-center" style="width:60%">
         
         <input type="text" name="eventId" value="{{$eventId}}" placeholder="Input event ID" class="form-control mr-sm-2" style="width:85%">
         <button type="submit" class="btn btn-primary my-2 my-sm-0">Search</button>
     </form>
     
     @if($event)
-        <!-- <div class="row"> -->
-            <h5>Created by : {{$event->user->name}}</h5>
-        <!-- </div> -->
-        <div class="row justify-content-center">
-            <h3>{{$event->title}}</h3>
-        </div>
-        <div class="row justify-content-center">
-            <h5>{{$event->description}}</h3>
-        </div>
+        <h5>Created by : {{$event->user->name}}</h5>
+        @if(isCreator)
+        <form method="POST" action="/update-guestbook">
+            @csrf
 
-        @if($acceptance)
-            <form method="POST" action="/sign-guestbook" class="form-inline my-2 justify-content-between">
-                @csrf
-                <!-- The value will be changed later !!! -->
-                <input type="text" class="form-control col-md-3" value="{{$user->name}}" readonly>
-                <input type="text" class="form-control col-md-4" name="address" value="{{$user->address}}" placeholder ="Address">
-                <input type="text" class="form-control col-md-4" name="message" value="" placeholder="Message">
-                <button type="submit" class="btn btn-primary my-2 my-sm-0"><span class="fas fa-check"></span></button>
-                <input type="hidden" name="eventId" value={{$eventId}}>
-            </form>
-        @else
-            <div class="row justify-content-center">
-                <h3>Sorry, The event is currently not accepting any sign</h3>
+            <div class="form-group row">
+                <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Event Name') }}</label>
+
+                <div class="col-md-6">
+                    <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{$event->title}}" required autofocus>
+
+                    @error('title')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
             </div>
-        @endif 
-        
+
+            <div class="form-group row">
+                <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Description') }}</label>
+
+                <div class="col-md-6">
+                    <textarea id="description" type="text" class="form-control @error('description') is-invalid @enderror" name="description" value="{{$event->description}}"></textarea>
+                </div>
+            </div>
+
+            <div class="form-group row mb-0">
+                <div class="col-md-6 offset-md-4">
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('Update') }} 
+                    </button>
+                </div>
+            </div>
+        </form>
+
+        @else
+            <h5>Created by : {{$event->user->name}}</h5>
+            <div class="row justify-content-center">
+                <h3>{{$event->title}}</h3>
+            </div>
+            <div class="row justify-content-center">
+                <h5>{{$event->description}}</h3>
+            </div>
+
+            @if($acceptance)
+                <form method="POST" action="/sign-guestbook" class="form-inline my-2 justify-content-between">
+                    @csrf
+                    <!-- The value will be changed later !!! -->
+                    <input type="text" class="form-control col-md-3" value="{{$user->name}}" readonly>
+                    <input type="text" class="form-control col-md-4" name="address" value="{{$user->address}}" placeholder ="Address">
+                    <input type="text" class="form-control col-md-4" name="message" value="" placeholder="Message">
+                    <button type="submit" class="btn btn-primary my-2 my-sm-0"><span class="fas fa-check"></span></button>
+                    <input type="hidden" name="eventId" value={{$eventId}}>
+                </form>
+            @else
+                <div class="row justify-content-center">
+                    <h3>Sorry, The event is currently not accepting any sign</h3>
+                </div>
+            @endif 
+        @endif
     @endif
 </div>
 @endsection
