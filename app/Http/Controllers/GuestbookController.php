91c9e75d15c9a315a42c53802bd7ba7f9a00ec8e
foster0400 <chanchan0400@gmail.com>
2021-08-03 12:02:42 +0700

    tes

--- app/Http/Controllers/GuestbookController.php
+++ app/Http/Controllers/GuestbookController.php
@@ -35,7 +35,7 @@ class GuestbookController extends Controller
         $this->validate($request, [
             'title' => ['required', 'string', 'max:255'],
         ]);
-        dd($request->description);
+        // dd($request->description);
         Event::create([
             'title' => $request->title,
             'description' => $request->description,
