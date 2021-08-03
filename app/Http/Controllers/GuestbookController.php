05f37ae162d542f354006144d244ad72a1c5f218
foster0400 <chanchan0400@gmail.com>
2021-08-03 11:47:35 +0700

    update guestbook controller

--- app/Http/Controllers/GuestbookController.php
+++ app/Http/Controllers/GuestbookController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Event;
+use App\Sign;
+
 
 class GuestbookController extends Controller
 {
     //
     public function search(Request $request)
     {
+        $user = Auth()->user;
         $eventId = $request->eventId;
         $event=NULL;
         $message=NULL;
@@ -25,8 +28,9 @@ class GuestbookController extends Controller
                 $message="NOT FOUND";
             }
         }
-        return view('sign', ['event' => $event,'eventId'=>$eventId,'message'=>$message,'acceptance'=>$acceptance]);
+        return view('sign', ['user' => $user,'event' => $event,'eventId'=>$eventId,'message'=>$message,'acceptance'=>$acceptance]);
     }
+
     public function create(Request $request){
         $this->validate($request, [
             'title' => ['required', 'string', 'max:255'],
@@ -37,5 +41,27 @@ class GuestbookController extends Controller
             'creator_id' => Auth::user()->id,
         ]);
         return redirect()->back();
+    }
+//new
+    public function viewAll(){
+        $myEvents = Event::where('creator_id',Auth()->user()->id);
+    }
+
+    public function viewGB($id){
+        $event = Event::where('id',$id)->first;
+        $guest = Sign::where('event_id',$id)->paginate(10);
+    }
+
+    public function myProfile(){
+        $my = Auth()->user();
+        return view('profile', ['my' => $my]);
+    }
+    public function updateProfile(Request $request){
+        $this->validate($request, [
+            'address' => ['string', 'max:100'],
+        ]);
+        // User::where(update([
+
+        // ])
     }
 }
