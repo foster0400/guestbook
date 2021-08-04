0ef02d25361d6ab12e93e8905a35c2aa2587ec67
foster0400 <chanchan0400@gmail.com>
2021-08-04 14:22:44 +0700

    tes sign

--- resources/views/sign.blade.php
+++ resources/views/sign.blade.php
@@ -16,7 +16,9 @@
         }
     }
     @media only screen and (min-width: 691px) {
-        
+        .identity{
+            margin-left: 3px;
+        }
         .btn-right{
             margin-left: none;
         }
