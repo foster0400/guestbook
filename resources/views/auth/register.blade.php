97d3f26c126f6799afdd0d6a73b36e34e3e055a1
foster0400 <chanchan0400@gmail.com>
2021-08-04 16:40:45 +0700

    update register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,7 +21,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">
+                <div class="card-header text-center">
                     <h3>{{ __('Register') }}</h3>
                 </div>
                 
