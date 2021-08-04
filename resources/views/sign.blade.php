e789b0b241637249f7a2e5d813ec2202136ac14f
foster0400 <chanchan0400@gmail.com>
2021-08-04 13:53:46 +0700

    tes sign

--- resources/views/sign.blade.php
+++ resources/views/sign.blade.php
@@ -4,7 +4,8 @@
         /* width: 100%; */
         margin:auto;
     }
-    @media only screen and (min-width: 700px) {
+    
+    @media only screen and (min-width: 576px) {
         .identity .form-control {
             display: block!important;
             width: 100%!important;
@@ -14,11 +15,11 @@
             margin-left: auto;
         }
     }
-    @media only screen and (max-width: 700px) {
+    @media only screen and (min-width: 691px) {
         .identity .form-control {
-            display: block!important;
-            width: 100%!important;
-            margin-bottom: 10px;
+            display: inline-block;
+            width: auto;
+            vertical-align: middle;
         }
         .btn-right{
             margin-left: auto;
