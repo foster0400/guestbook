8a0261be215cad101cdb2124587e7c3403de974d
foster0400 <chanchan0400@gmail.com>
2021-08-04 09:50:34 +0700

    update all gb view

--- resources/views/allGB.blade.php
+++ resources/views/allGB.blade.php
@@ -11,19 +11,19 @@
                 <div class = "card-body bg-white">
                 @if(!$events->isEmpty())
                     <div class="row justify-content-center bg-dark text-white">
-                        <div class="col-sm-2 center-block text-center">
+                        <div class="col-sm-3 center-block text-center">
                             <strong>Event ID</strong>
                         </div>
-                        <div class="col-sm-3 center-block text-center">
+                        <div class="col-sm-5 center-block text-center">
                             <strong>Event Title</strong>
                         </div>
                     </div>
                     @foreach($events as $event)
                         <div class="row justify-content-center">
-                            <div class="col-sm-2 center-block text-center">
+                            <div class="col-sm-3 center-block text-center">
                                 <strong>{{$event->id}}</strong>
                             </div>
-                            <div class="col-sm-3 center-block text-center">
+                            <div class="col-sm-5 center-block text-center">
                                 <strong>{{$event->title}}</strong>
                             </div>
                         </div>
