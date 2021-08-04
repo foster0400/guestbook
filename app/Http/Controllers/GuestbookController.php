1253e08f908697c8dd8d4bf0092775a9e7a13315
foster0400 <chanchan0400@gmail.com>
2021-08-04 09:02:34 +0700

    update changemode function

--- app/Http/Controllers/GuestbookController.php
+++ app/Http/Controllers/GuestbookController.php
@@ -97,7 +97,7 @@ class GuestbookController extends Controller
         }else{
             $onoff = true;
         }
-        $event = Event::where('id',$request->id)->update([
+        $event = Event::where('eventId',$request->id)->update([
             'isOpened' => $onoff,
         ]);
         return redirect()->back();
