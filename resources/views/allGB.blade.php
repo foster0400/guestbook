8c39e3d16b4e235860a5be97b961abc75644a452
foster0400 <chanchan0400@gmail.com>
2021-08-04 09:36:22 +0700

    update all gb view

--- resources/views/allGB.blade.php
+++ resources/views/allGB.blade.php
@@ -1,7 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+<div class="container">
+    <div class = "row justify-content-center">
+        <div class = "col-md">
+            <div class="card">
+                <div class = "card-header">{{__('My Guestbook')}}</div>
+                <div class = "card-body bg-white">
+                @if(!$events->isEmpty())
+                    <div class="row justify-content-center">
+                        <div class="col-sm-3 center-block text-center"style="border:1px solid black">
+                            <strong>Event ID</strong>
+                        </div>
+                        <div class="col-sm-5 center-block text-center"style="border:1px solid black">
+                            <strong>Event Title</strong>
+                        </div>
+                    </div>
+                    @foreach($events as $event)
+                        <div class="row justify-content-center">
+                            <div class="col-sm-3"style="border:1px solid black">
+                                <strong>{{$event->id}}</strong>
+                            </div>
+                            <div class="col-sm-5"style="border:1px solid black">
+                                <strong>{{$event->title}}</strong>
+                            </div>
+                        </div>
+                    @endforeach
+                @else
+                    <h5>You have not created any guestbook yet, <a href="/create-guestbook">click here to create one</a></h5>
+                @endif
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+    <!-- <div class="container">
         <h3 style="text-align:center; padding-bottom:25px;padding-top:25px">My Guestbook</h3>
         @if(!$events->isEmpty())
             <div class="row justify-content-center">
@@ -25,5 +59,5 @@
         @else
             <h5>You have not created any guestbook yet, <a href="/create-guestbook">click here to create one</a></h5>
         @endif
-    </div>
+    </div> -->
 @endsection
