524a0ae90072e4e5b2cf0dbfc928990fe724fab5
foster0400 <chanchan0400@gmail.com>
2021-08-04 08:44:26 +0700

    update sign view

--- resources/views/sign.blade.php
+++ resources/views/sign.blade.php
@@ -46,6 +46,7 @@
                 </div>
 
                 <div class="form-group row">
+                    <input type="hidden" name="eventId" value="{{$event->id}}">
                     <label for="status" class="col-md-4 col-form-label text-md-right">{{ __('Signing status') }}</label>
                     @if($acceptance)
                         <div class="col-md-6">
